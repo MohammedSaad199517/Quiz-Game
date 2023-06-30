83c9160842327bbb224f3c42edcb40ffdf1486ed
Mohammed Saad <mohammedsaad20161995@gmail.com>
2023-06-30 20:07:18 +0300

    add ConfigurationUISate

---
+++ app/src/main/java/com/mohammed/quizgame/ui/screens/configuration/ConfigurationUISate.kt
@@ -0,0 +1,7 @@
+package com.mohammed.quizgame.ui.screens.configuration
+
+data class ConfigurationUISate(
+    val response:ConfigurationUISate ? = null,
+    val isLoading: Boolean = false,
+    val errors: String? = null
+)
