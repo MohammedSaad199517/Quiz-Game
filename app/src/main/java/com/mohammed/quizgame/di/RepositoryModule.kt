121f88439876e50b64d2af496533947848fbfe60
Mohammed Saad <mohammedsaad20161995@gmail.com>
2023-06-30 20:10:17 +0300

    add RepositoryModule

---
+++ app/src/main/java/com/mohammed/quizgame/di/RepositoryModule.kt
@@ -0,0 +1,19 @@
+package com.mohammed.quizgame.di
+
+import com.mohammed.quizgame.data.repository.QuizGameRepositoryImp
+import com.mohammed.quizgame.data.service.ApiService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RepositoryModule {
+
+    @Singleton
+    @Provides
+    fun provideQuizGameRepository(apiService: ApiService) = QuizGameRepositoryImp(apiService)
+
+}
