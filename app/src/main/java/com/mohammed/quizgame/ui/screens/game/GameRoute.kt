7843852b22d61f411e573841a0ac25c42c3e47bc
Mohammed Saad <mohammedsaad20161995@gmail.com>
2023-07-01 23:33:29 +0300

    add gameRoute

---
+++ app/src/main/java/com/mohammed/quizgame/ui/screens/game/GameRoute.kt
@@ -0,0 +1,11 @@
+package com.mohammed.quizgame.ui.screens.game
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import com.mohammed.quizgame.Screen
+
+
+fun NavGraphBuilder.gameRoute(navController: NavController){
+    composable(Screen.GameScreen.route) { GameScreen(navController) }
+}
