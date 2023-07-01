db3908d0101fd6f941c047fdcf765efd68accf75
Mohammed Saad <mohammedsaad20161995@gmail.com>
2023-07-01 11:56:04 +0300

    add toConfiguration extension function

--- app/src/main/java/com/mohammed/quizgame/utils/Extensions.kt
+++ app/src/main/java/com/mohammed/quizgame/utils/Extensions.kt
@@ -1,2 +1,12 @@
 package com.mohammed.quizgame.utils
 
+import com.mohammed.quizgame.data.response.QuestionsItem
+import com.mohammed.quizgame.domain.models.Configuration
+
+fun QuestionsItem.toConfiguration():Configuration{
+    return Configuration(
+        category = category!!,
+        level = difficulty!!
+
+    )
+}
