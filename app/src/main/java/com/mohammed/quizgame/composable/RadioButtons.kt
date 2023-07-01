6af33da942c01ed434039bb57683a8265aa65428
Mohammed Saad <mohammedsaad20161995@gmail.com>
2023-07-01 19:58:40 +0300

    get SelectedLevel value  in viewModel

--- app/src/main/java/com/mohammed/quizgame/composable/RadioButtons.kt
+++ app/src/main/java/com/mohammed/quizgame/composable/RadioButtons.kt
@@ -12,29 +12,38 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.mohammed.quizgame.ui.screens.configuration.ConfigurationViewModel
 import com.mohammed.quizgame.ui.theme.mainColor
 
-object Level{
+object Level {
     const val EASY = "Easy"
     const val MEDIUM = "Medium"
     const val HARD = "Hard"
 }
 
 @Composable
-fun Option(selectLevel: MutableState<String>,level:String){
+fun Option(
+    selectLevel: MutableState<String>,
+    level: String,
+    viewModel: ConfigurationViewModel
+) {
     RadioButton(
-        selected = selectLevel.value == level ,
-        onClick = { selectLevel.value = level },
+        selected = selectLevel.value == level,
+        onClick = {
+            selectLevel.value = level
+            viewModel.updateSelectedLevel(selectLevel.value)
+        },
         colors = RadioButtonDefaults.colors(mainColor),
 
-    )
-//    Spacer(modifier = Modifier.size(16.dp))
-    Text(text = level,  modifier = Modifier.padding(0.dp,14.dp))
+        )
+    Text(text = level, modifier = Modifier.padding(0.dp, 14.dp))
 }
+
 @Composable
-fun RadioButtons() {
+fun RadioButtons(
+    viewModel: ConfigurationViewModel
+) {
 
     val selectLevel = remember { mutableStateOf("") }
 
@@ -42,16 +51,11 @@ fun RadioButtons() {
         modifier = Modifier.fillMaxWidth(),
         Arrangement.Center
     ) {
-        Option(selectLevel,Level.EASY)
-        Option(selectLevel,Level.MEDIUM)
-        Option(selectLevel,Level.HARD)
+        Option(selectLevel, Level.EASY,viewModel)
+        Option(selectLevel, Level.MEDIUM,viewModel)
+        Option(selectLevel, Level.HARD,viewModel)
     }
 
 }
 
-@Preview(showSystemUi = true)
-@Composable
-fun previewRadioButtons() {
-    RadioButtons()
-}
 
