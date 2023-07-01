ecba80bc3561527acdf147f3d0dfaa56385ea51c
Mohammed Saad <mohammedsaad20161995@gmail.com>
2023-07-01 11:58:25 +0300

    make fun getAllQuestions return Result<List<Configuration>>

--- app/src/main/java/com/mohammed/quizgame/data/repository/QuizGameRepository.kt
+++ app/src/main/java/com/mohammed/quizgame/data/repository/QuizGameRepository.kt
@@ -1,10 +1,9 @@
 package com.mohammed.quizgame.data.repository
 
-import com.mohammed.quizgame.data.response.Questions
-import retrofit2.Response
+import com.mohammed.quizgame.domain.models.Configuration
 
 
 interface QuizGameRepository{
-    suspend fun getAllQuestions():Response<Questions>
+    suspend fun getAllQuestions():Result<List<Configuration>>
 
 }
