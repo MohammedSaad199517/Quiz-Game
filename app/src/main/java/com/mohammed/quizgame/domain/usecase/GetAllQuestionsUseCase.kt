6425ec60e3e525e0135089a9d021176a684a16d7
Mohammed Saad <mohammedsaad20161995@gmail.com>
2023-07-01 11:56:57 +0300

    update GetAllQuestionsUseCase

--- app/src/main/java/com/mohammed/quizgame/domain/usecase/GetAllQuestionsUseCase.kt
+++ app/src/main/java/com/mohammed/quizgame/domain/usecase/GetAllQuestionsUseCase.kt
@@ -1,10 +1,11 @@
 package com.mohammed.quizgame.domain.usecase
 
-import com.mohammed.quizgame.data.repository.QuizGameRepositoryImp
+import com.mohammed.quizgame.data.repository.QuizGameRepository
 import javax.inject.Inject
 
 class GetAllQuestionsUseCase @Inject constructor(
-   private val quizGameRepository: QuizGameRepositoryImp
+    private val quizGameRepository: QuizGameRepository
 ) {
-    suspend operator fun invoke() = quizGameRepository.getAllQuestions()
+    suspend operator fun invoke() = quizGameRepository.getAllQuestions().getOrDefault(emptyList())
+
 }
