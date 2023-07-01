42a1a8fe397fd6072051713b8a7fa6811d010da5
Mohammed Saad <mohammedsaad20161995@gmail.com>
2023-07-01 19:08:44 +0300

    update ConfigurationUISate

--- app/src/main/java/com/mohammed/quizgame/ui/screens/configuration/ConfigurationUISate.kt
+++ app/src/main/java/com/mohammed/quizgame/ui/screens/configuration/ConfigurationUISate.kt
@@ -2,8 +2,12 @@ package com.mohammed.quizgame.ui.screens.configuration
 
 import com.mohammed.quizgame.domain.models.Configuration
 
+
 data class ConfigurationUISate(
-    val response: List<Configuration> ? = null,
+    val categories: List<Configuration> = emptyList(),
+    val selectedCategory: String = "",
+    val selectedLevel: String = "",
+    val selectedQuantity: Int =0,
     val isLoading: Boolean = true,
     val errors: String? = null
 
