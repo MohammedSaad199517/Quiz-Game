ad35c843146df847cf21bf2a5f9168dc222c430d
Mohammed Saad <mohammedsaad20161995@gmail.com>
2023-07-01 19:11:28 +0300

    add toQuizGameEntity extension function

--- app/src/main/java/com/mohammed/quizgame/utils/Extensions.kt
+++ app/src/main/java/com/mohammed/quizgame/utils/Extensions.kt
@@ -1,12 +1,25 @@
 package com.mohammed.quizgame.utils
 
-import com.mohammed.quizgame.data.response.QuestionsItem
+import com.mohammed.quizgame.data.local.QuizGameEntity
+import com.mohammed.quizgame.data.remote.dto.QuestionsItemDto
 import com.mohammed.quizgame.domain.models.Configuration
 
-fun QuestionsItem.toConfiguration():Configuration{
-    return Configuration(
-        category = category!!,
-        level = difficulty!!
+fun QuestionsItemDto.toQuizGameEntity(): QuizGameEntity {
 
+    return QuizGameEntity(
+        questionId = id!!,
+        category = category!!,
+        correctAnswer = correctAnswer!!,
+        firstIncorrectAnswers = incorrectAnswers?.get(0)!!,
+        secondIncorrectAnswers = incorrectAnswers[1]!!,
+        thirdIncorrectAnswers = incorrectAnswers[2]!!,
+        difficulty = difficulty!!,
+        question = question?.text!!
+    )
+}
+
+fun QuizGameEntity.toConfiguration(): Configuration {
+    return Configuration(
+        category = category
     )
 }
