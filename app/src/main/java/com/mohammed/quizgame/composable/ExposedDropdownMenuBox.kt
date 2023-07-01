f94feb143181d2f1721233534b96cce1e0c35262
Mohammed Saad <mohammedsaad20161995@gmail.com>
2023-07-01 20:01:43 +0300

    use updateSelectedCategory

--- app/src/main/java/com/mohammed/quizgame/composable/ExposedDropdownMenuBox.kt
+++ app/src/main/java/com/mohammed/quizgame/composable/ExposedDropdownMenuBox.kt
@@ -5,73 +5,77 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import com.mohammed.quizgame.ui.screens.configuration.ConfigurationViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ExposedDropdownMenuBox(
     options: List<String>,
-    width: Dp = 100.dp
+    width: Dp = 100.dp,
+    viewModel: ConfigurationViewModel
+
 ) {
+    val isLoading by viewModel.uiState.collectAsState()
+
+    if (!isLoading.isLoading) {
+        var expandedState by remember { mutableStateOf(false) }
+        var selectedOption by remember { mutableStateOf(options[0]) }
 
 
-    var expandedState by remember { mutableStateOf(false) }
-    var selectedOption by remember { mutableStateOf(options[0]) }
-    Box(
-        modifier = Modifier.fillMaxWidth(),
-        contentAlignment = Alignment.Center
+        Box(
+            modifier = Modifier.fillMaxWidth(),
+            contentAlignment = Alignment.Center
 
-    )
-    {
-        androidx.compose.material3.ExposedDropdownMenuBox(
-            modifier = Modifier.width(width),
-            expanded = expandedState,
-            onExpandedChange = { expandedState = !expandedState })
+        )
         {
-            TextField(
-                value = selectedOption,
-                onValueChange = {},
-                readOnly = true,
-                trailingIcon = {
-                    ExposedDropdownMenuDefaults.TrailingIcon(expanded = expandedState)
-                },
-                colors = ExposedDropdownMenuDefaults.textFieldColors(),
-                modifier = Modifier.menuAnchor()
-            )
-
-            ExposedDropdownMenu(
+            ExposedDropdownMenuBox(
+                modifier = Modifier.width(width),
                 expanded = expandedState,
-                onDismissRequest = { expandedState = false }
-            ) {
-                options?.forEach { selectedText ->
-                    DropdownMenuItem(
-                        text = { Text(text = selectedText) },
-                        onClick = {
-                            selectedOption = selectedText
-                            expandedState = false
-                        })
+                onExpandedChange = { expandedState = !expandedState })
+            {
+                TextField(
+                    value = selectedOption,
+                    onValueChange = {},
+                    readOnly = true,
+                    trailingIcon = {
+                        ExposedDropdownMenuDefaults.TrailingIcon(expanded = expandedState)
+                    },
+                    colors = ExposedDropdownMenuDefaults.textFieldColors(),
+                    modifier = Modifier.menuAnchor()
+                )
+
+                ExposedDropdownMenu(
+                    expanded = expandedState,
+                    onDismissRequest = { expandedState = false }
+                ) {
+
+                    options?.forEach { selectedText ->
+                        DropdownMenuItem(
+                            text = { Text(text = selectedText) },
+                            onClick = {
+                                selectedOption = selectedText
+                                expandedState = false
+                                viewModel.updateSelectedCategory(selectedOption)
+                            })
+                    }
                 }
             }
         }
     }
-}
-
-@Preview(showSystemUi = true)
-@Composable
-fun PreviewExposedDropdownMenuBox() {
-    val optionsNumbers = listOf("1", "2", "3")
-    ExposedDropdownMenuBox(optionsNumbers)
 
 }
+
