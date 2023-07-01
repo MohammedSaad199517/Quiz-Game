ad6888b1aa23a643c9fa026932b742d78bf9d908
Mohammed Saad <mohammedsaad20161995@gmail.com>
2023-07-01 11:58:56 +0300

    add getAllQuestions

--- app/src/main/java/com/mohammed/quizgame/data/repository/QuizGameRepositoryImp.kt
+++ app/src/main/java/com/mohammed/quizgame/data/repository/QuizGameRepositoryImp.kt
@@ -1,13 +1,30 @@
 package com.mohammed.quizgame.data.repository
 
 import com.mohammed.quizgame.data.service.ApiService
+import com.mohammed.quizgame.domain.models.Configuration
+import com.mohammed.quizgame.utils.toConfiguration
 import javax.inject.Inject
 
 
 class QuizGameRepositoryImp  @Inject constructor(
    private val apiService: ApiService
 ):QuizGameRepository  {
-    override suspend fun getAllQuestions()= apiService.getAllQuestion()
+    override suspend fun getAllQuestions():Result<List<Configuration>>{
+        try {
+            val response = apiService.getAllQuestion()
+            if (response.isSuccessful){
+                val items = response.body()?.map { it.toConfiguration() }
+                items?.let {
+                    return Result.success(items)
+                }
+
+            }
+            return Result.failure(Exception("Failed to retrieve data"))
+        }catch (e:Exception){
+            return Result.failure(e)
+        }
+
+    }
 
 
 
