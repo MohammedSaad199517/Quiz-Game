ba906cb23cde9c0024b0da5589b3575b4f8385ca
Mohammed Saad <mohammedsaad20161995@gmail.com>
2023-07-01 23:33:49 +0300

    add Constants

---
+++ app/src/main/java/com/mohammed/quizgame/utils/Constants.kt
@@ -0,0 +1,9 @@
+package com.mohammed.quizgame.utils
+
+object Constants {
+    const val CONFIGURATION ="configuration"
+    const val    SELECTED_CATEGORY = "selectedCategory"
+    const val    SELECTED_LEVEL = "selectedLevel"
+    const val    SELECTED_QUANTITY = "selectedQuantity"
+
+}
