e14aa603932648c0fc63367f98cea96238237298
Mohammed Saad <mohammedsaad20161995@gmail.com>
2023-07-01 19:06:43 +0300

    add data class QuizGameEntity

---
+++ app/src/main/java/com/mohammed/quizgame/data/local/QuizGameEntity.kt
@@ -0,0 +1,17 @@
+package com.mohammed.quizgame.data.local
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "QUIZ_GAME_TABLE")
+data class QuizGameEntity (
+    @PrimaryKey
+    val questionId: String,
+    val category: String,
+    val correctAnswer: String,
+    val firstIncorrectAnswers: String,
+    val secondIncorrectAnswers: String,
+    val thirdIncorrectAnswers: String,
+    val question: String,
+    val difficulty: String
+)
