a32b8aff3c72977ca1994a4590482c439e185a8e
Mohammed Saad <mohammedsaad20161995@gmail.com>
2023-07-01 11:54:07 +0300

    use viewModel

--- app/src/main/java/com/mohammed/quizgame/ui/screens/configuration/ConfigurationScreen.kt
+++ app/src/main/java/com/mohammed/quizgame/ui/screens/configuration/ConfigurationScreen.kt
@@ -1,6 +1,7 @@
 package com.mohammed.quizgame.ui.screens.configuration
 
 import android.annotation.SuppressLint
+import android.util.Log
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,11 +9,14 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.mohammed.quizgame.R
@@ -23,33 +27,47 @@ import com.mohammed.quizgame.composable.TextSelect
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun ConfigurationScreen(navController: NavController) {
+fun ConfigurationScreen(
+    navController: NavController,
+    viewModel: ConfigurationViewModel = hiltViewModel()
+) {
     val options = listOf("one", "two", "three")
     val optionsNumbers = listOf("1", "2", "3")
+    val questions by viewModel.uiState.collectAsState()
+    val categories = questions.response?:"no data"
+//    .map { it.category }
+
+
+    Log.i("testScreen", "$categories")
 
     Column(
         modifier = Modifier.fillMaxSize()
 
     ) {
         Spacer(modifier = Modifier.height(32.dp))
-        TextSelect(text= stringResource(R.string.select_question_category))
+        TextSelect(text = stringResource(R.string.select_question_category))
+
         Spacer(modifier = Modifier.height(8.dp))
-        ExposedDropdownMenuBox(options = options,250.dp)
+        ExposedDropdownMenuBox(options = options, 250.dp)
+
         Spacer(modifier = Modifier.height(32.dp))
-        TextSelect(text= stringResource(R.string.select_difficult_level))
+        TextSelect(text = stringResource(R.string.select_difficult_level))
+
         Spacer(modifier = Modifier.height(8.dp))
         RadioButtons()
+
         Spacer(modifier = Modifier.height(32.dp))
         TextSelect(text = stringResource(R.string.select_question_quantity))
+
         ExposedDropdownMenuBox(options = optionsNumbers)
         Spacer(modifier = Modifier.height(32.dp))
         Button(
 
             modifier = Modifier.align(Alignment.CenterHorizontally),
-            onClick = {  },
+            onClick = { },
 
-        ) {
-            Text(text = "Start"  )
+            ) {
+            Text(text = "Start")
         }
     }
 }
@@ -58,5 +76,8 @@ fun ConfigurationScreen(navController: NavController) {
 @Preview(showSystemUi = true)
 fun PreviewConfigurationScreen() {
     val navController = rememberNavController()
-    ConfigurationScreen(navController)
+    ConfigurationScreen(
+        navController
+
+    )
 }
