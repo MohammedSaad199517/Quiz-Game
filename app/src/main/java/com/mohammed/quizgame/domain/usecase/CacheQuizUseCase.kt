f06fbda5f0af693b066ef9ba9138be94f17fbbe4
Mohammed Saad <mohammedsaad20161995@gmail.com>
2023-07-02 14:06:52 +0300

    change repository injection type

--- app/src/main/java/com/mohammed/quizgame/domain/usecase/CacheQuizUseCase.kt
+++ app/src/main/java/com/mohammed/quizgame/domain/usecase/CacheQuizUseCase.kt
@@ -1,10 +1,10 @@
 package com.mohammed.quizgame.domain.usecase
 
-import com.mohammed.quizgame.data.repository.QuizGameRepository
+import com.mohammed.quizgame.data.repository.quiz.QuizRepository
 import javax.inject.Inject
 
 class CacheQuizUseCase @Inject constructor(
-    private val quizGameRepository: QuizGameRepository
+    private val quizGameRepository: QuizRepository
 ) {
     suspend operator fun invoke() = quizGameRepository.fetchQuizAndCache()
 }
