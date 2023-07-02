973a0896c9b7a3334c8618e90c7356a29597cfc2
Mohammed Saad <mohammedsaad20161995@gmail.com>
2023-07-02 18:36:17 +0300

    update GameUISate

--- app/src/main/java/com/mohammed/quizgame/ui/screens/game/GameUISate.kt
+++ app/src/main/java/com/mohammed/quizgame/ui/screens/game/GameUISate.kt
@@ -1,12 +1,14 @@
 package com.mohammed.quizgame.ui.screens.game
 
-data class GameUISate (
-    val question:List<String> = emptyList(),
-    val totalQuestions:Int =0,
-    val currentQuestion:Int =0,
-    val correctAnswer:String ="",
-    val wrongAnswer:List<String> = emptyList(),
+data class GameUISate(
+    val question: String = "",
+    val time: Int = 0,
+    val totalQuestions: Int = 0,
+    val currentQuestion: Int = 0,
+    val currentScore: Int = 0,
+    val correctAnswer: String = "",
+    val wrongAnswer: List<String> = emptyList(),
     val isLoading: Boolean = true,
     val errors: String? = null
 
-        )
+)
