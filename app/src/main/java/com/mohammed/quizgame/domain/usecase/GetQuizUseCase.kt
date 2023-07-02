a384279879b19f2f23e55d8c653972ef77d63edb
Mohammed Saad <mohammedsaad20161995@gmail.com>
2023-07-02 14:15:13 +0300

    add GetQuizUseCase

---
+++ app/src/main/java/com/mohammed/quizgame/domain/usecase/GetQuizUseCase.kt
@@ -0,0 +1,11 @@
+package com.mohammed.quizgame.domain.usecase
+
+import com.mohammed.quizgame.data.repository.quiz.QuizRepository
+import com.mohammed.quizgame.utils.toGame
+import javax.inject.Inject
+
+class GetQuizUseCase @Inject constructor(
+   private val quizRepository: QuizRepository
+) {
+    suspend operator fun invoke()= quizRepository.getAllCachedQuestions().map { it.toGame()}
+}
