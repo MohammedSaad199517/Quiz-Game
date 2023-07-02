2fe17a3ceff3d113fe7457ab42a97810a174f5a9
Mohammed Saad <mohammedsaad20161995@gmail.com>
2023-07-02 14:15:23 +0300

    add GetSavedConfigurationUseCase

---
+++ app/src/main/java/com/mohammed/quizgame/domain/usecase/GetSavedConfigurationUseCase.kt
@@ -0,0 +1,12 @@
+package com.mohammed.quizgame.domain.usecase
+
+import com.mohammed.quizgame.data.repository.configuration.ConfigurationRepository
+import com.mohammed.quizgame.data.repository.configuration.SavedConfiguration
+import javax.inject.Inject
+
+class GetSavedConfigurationUseCase @Inject constructor(
+    private val configurationRepository: ConfigurationRepository) {
+    suspend operator fun invoke(): SavedConfiguration? =
+         configurationRepository.getSavedConfiguration()
+
+}
