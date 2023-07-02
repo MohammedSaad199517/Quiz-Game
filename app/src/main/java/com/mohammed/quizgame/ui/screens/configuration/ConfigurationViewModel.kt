a82781d3738aad43e2a36a885e8eb9a769eadbf5
Mohammed Saad <mohammedsaad20161995@gmail.com>
2023-07-02 14:08:58 +0300

    use  saveConfigurationUseCase

--- app/src/main/java/com/mohammed/quizgame/ui/screens/configuration/ConfigurationViewModel.kt
+++ app/src/main/java/com/mohammed/quizgame/ui/screens/configuration/ConfigurationViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.mohammed.quizgame.domain.usecase.CacheQuizUseCase
 import com.mohammed.quizgame.domain.usecase.GetAllCategoryUseCase
+import com.mohammed.quizgame.domain.usecase.SaveConfigurationUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -15,10 +16,11 @@ import javax.inject.Inject
 @HiltViewModel
 class ConfigurationViewModel @Inject constructor(
     private val getAllCategoryUseCase: GetAllCategoryUseCase,
-    private val cacheQuizUseCase: CacheQuizUseCase
-) :ViewModel() {
+    private val cacheQuizUseCase: CacheQuizUseCase,
+    private val saveConfigurationUseCase: SaveConfigurationUseCase,
+) : ViewModel() {
     private val _uiState = MutableStateFlow(ConfigurationUISate())
-    val uiState:StateFlow<ConfigurationUISate> =_uiState
+    val uiState: StateFlow<ConfigurationUISate> = _uiState
 
 
     init {
@@ -29,24 +31,46 @@ class ConfigurationViewModel @Inject constructor(
             getAllQuestions()
         }
     }
-    suspend fun getAllQuestions(){
 
-            _uiState.update {
-                it.copy(
-                    isLoading = false,
-                    categories = getAllCategoryUseCase.invoke()
+    suspend fun getAllQuestions() {
 
-                )
-            }
+        _uiState.update {
+            it.copy(
+                isLoading = false,
+                categories = getAllCategoryUseCase.invoke()
+
+            )
+        }
 
     }
+
     fun updateSelectedCategory(value: String) {
         _uiState.update { it.copy(selectedCategory = value) }
     }
+
     fun updateSelectedLevel(value: String) {
-       _uiState.update { it.copy(selectedLevel = value) }
+        _uiState.update { it.copy(selectedLevel = value) }
     }
+
     fun updateSelectedQuantity(value: Int) {
         _uiState.update { it.copy(selectedQuantity = value) }
     }
+
+    fun saveConfig(
+        selectedCategory: String,
+        selectedLevel: String,
+        selectedQuantity: Int
+    ) {
+        viewModelScope.launch {
+            saveConfigurationUseCase.invoke(
+                selectedCategory,
+                selectedLevel,
+                selectedQuantity
+            )
+        }
+
+    }
+
+
+
 }
