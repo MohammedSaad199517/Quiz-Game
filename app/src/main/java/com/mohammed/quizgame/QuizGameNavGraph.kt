175fe8623cbe4025b02be540b6cbf007fcaaf9f3
Mohammed Saad <mohammedsaad20161995@gmail.com>
2023-07-03 22:26:12 +0300

    call splashRoute

--- app/src/main/java/com/mohammed/quizgame/QuizGameNavGraph.kt
+++ app/src/main/java/com/mohammed/quizgame/QuizGameNavGraph.kt
@@ -5,16 +5,18 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.NavHostController
 import com.mohammed.quizgame.ui.screens.configuration.configurationRoute
 import com.mohammed.quizgame.ui.screens.game.gameRoute
+import com.mohammed.quizgame.ui.screens.splash.splashRoute
 import com.mohammed.quizgame.ui.screens.winner.winnerRoute
 
 
 @Composable
 fun QuizGameNavGraph (navController: NavHostController){
 
-    NavHost(navController = navController, startDestination = Screen.ConfigurationScreen.route) {
+    NavHost(navController = navController, startDestination = Screen.SplashScreen.route) {
 
         configurationRoute(navController)
         gameRoute(navController)
         winnerRoute(navController)
+        splashRoute(navController)
     }
 }
