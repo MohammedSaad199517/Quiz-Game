2a7d44bfd1ab2893c1c4f551aec4a2fa952ba43c
Mohammed Saad <mohammedsaad20161995@gmail.com>
2023-07-03 11:41:31 +0300

    update CurrentQuestion function

--- app/src/main/java/com/mohammed/quizgame/composable/CurrentQuestion.kt
+++ app/src/main/java/com/mohammed/quizgame/composable/CurrentQuestion.kt
@@ -1,39 +1,43 @@
 package com.mohammed.quizgame.composable
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.mohammed.quizgame.ui.theme.mainColor
-import com.mohammed.quizgame.ui.theme.white87
+import com.mohammed.quizgame.ui.theme.MainColor
+import com.mohammed.quizgame.ui.theme.White87
+
 
 @Composable
-fun CurrentQuestion(question: String) {
-    Text(
-        text = question,
-        modifier = Modifier
-            .fillMaxWidth()
-            .background(mainColor)
-            .padding(horizontal = 10.dp, vertical = 32.dp)
-            .height(150.dp),
-        textAlign = TextAlign.Center,
-        fontSize = 18.sp,
-        color = white87,
-        fontWeight = FontWeight.SemiBold
+fun CurrentQuestion(question: String, numberOfQuestion: String) {
 
-    )
+    Card(
+        colors = CardDefaults.cardColors(MainColor),
+        shape = RectangleShape
+    ) {
+        Text(
+            text = "Q$numberOfQuestion : $question",
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp)
+//            .padding( 16.dp)
+                .height(150.dp),
+            textAlign = TextAlign.Start,
+            fontSize = 18.sp,
+            color = White87,
+            fontWeight = FontWeight.SemiBold
+
+        )
+    }
+
 }
 
-@Composable
-@Preview(showSystemUi = true)
-fun PreviewCurrentQuestion() {
-    CurrentQuestion("wwwwwwwwwwwwwwwwwwwwwww")
-}
