b4893576203d76d1db88222d11b87b20bc3d8043
Mohammed Saad <mohammedsaad20161995@gmail.com>
2023-07-03 21:50:02 +0300

    add ProgressBar

---
+++ app/src/main/java/com/mohammed/quizgame/composable/ProgressBar.kt
@@ -0,0 +1,36 @@
+package com.mohammed.quizgame.composable
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ProgressBar() {
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        CircularProgressIndicator(
+            modifier = Modifier.padding(16.dp),
+            color = Color.Green,
+            strokeWidth = Dp(value = 4F)
+
+        )
+    }
+
+}
+@Composable
+@Preview(showSystemUi = true)
+fun PreviewProgressBar(){
+    ProgressBar()
+}
