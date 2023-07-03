0ceb3a56f4eeac14ebff4675734eee36651e5e9d
Mohammed Saad <mohammedsaad20161995@gmail.com>
2023-07-03 11:49:08 +0300

    add timer

--- app/src/main/java/com/mohammed/quizgame/ui/screens/game/GameViewModel.kt
+++ app/src/main/java/com/mohammed/quizgame/ui/screens/game/GameViewModel.kt
@@ -1,8 +1,8 @@
 package com.mohammed.quizgame.ui.screens.game
 
-import android.os.CountDownTimer
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.mohammed.quizgame.domain.usecase.CountdownTimerUseCase
 import com.mohammed.quizgame.domain.usecase.GetQuizUseCase
 import com.mohammed.quizgame.domain.usecase.GetSavedConfigurationUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -11,13 +11,13 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
-import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 @HiltViewModel
 class GameViewModel @Inject constructor(
     private val getQuizUseCase: GetQuizUseCase,
-    private val getSavedConfigurationUseCase: GetSavedConfigurationUseCase
+    private val getSavedConfigurationUseCase: GetSavedConfigurationUseCase,
+    private val countdownTimerUseCase: CountdownTimerUseCase
 
 
 ) : ViewModel() {
@@ -25,56 +25,62 @@ class GameViewModel @Inject constructor(
     private val _uiState = MutableStateFlow(GameUISate())
     val uiState: StateFlow<GameUISate> = _uiState
 
-    private var countDownTimer: CountDownTimer? = null
-    private val seconds = TimeUnit.SECONDS.toMillis(60)
-    var timeLeft = mutableSetOf(seconds)
 
-    val countDownInterval = 1000L
-    val timerText = mutableSetOf(timeLeft)
-
-//    fun startTimer() {
-//        viewModelScope.launch {
-//            countDownTimer = object : CountDownTimer(5_000, 1_000){
-//                override fun onTick(remaining: Long) {
-//                    _uiState.update { it.copy(time =remaining ) }
-//
-//                }
-//
-//                override fun onFinish() {
-//                    TODO("Not yet implemented")
-//                }
-//
-//            }
-//
-//        }
-//    }
-
-    fun updateTimerValue(timeRemain:Int){
-        viewModelScope.launch {
-            _uiState.update { it.copy(
-                time = timeRemain
-            ) }
-        }
-    }
     init {
         viewModelScope.launch(Dispatchers.IO) {
-//            _uiState.update {
-//                it.copy(
-//                    isLoading = false,
-//                    question = getQuizUseCase.invoke().map { it.question },
-//                    totalQuestions = getSavedConfigurationUseCase.invoke()?.selectedQuantity!!,
-//                    currentQuestion = 1,
-//                    correctAnswer = getQuizUseCase.invoke().get(0).correctAnswer,
-//                    wrongAnswer = listOf(
-//                        getQuizUseCase.invoke().get(0).firstIncorrectAnswers,
-//                        getQuizUseCase.invoke().get(0).secondIncorrectAnswers,
-//                        getQuizUseCase.invoke().get(0).thirdIncorrectAnswers,
-//                    )
-//
-//
-//                )
-//            }
+            _uiState.update {
+                it.copy(
+                    totalQuestions = getSavedConfigurationUseCase.invoke()?.selectedQuantity!!,
+                    question = getQuizUseCase.invoke().get(0).question,
+                    answers = getQuizUseCase.invoke().get(0).answers
+                )
+
+            }
+
         }
+
+
     }
 
+    fun startTimer() {
+        countdownTimerUseCase.startTimer(
+            totalTime = totalTime,
+            onTick = { secondsRemaining ->
+                viewModelScope.launch(Dispatchers.IO) {
+                    _uiState.update {
+                        it.copy(
+                            time = secondsRemaining
+                        )
+                    }
+                }
+            },
+            onFinish = {
+                viewModelScope.launch(Dispatchers.IO) {
+                    val currentQuestion = _uiState.value.currentQuestion
+                    if (currentQuestion < getSavedConfigurationUseCase.invoke()?.selectedQuantity!!) {
+                        _uiState.update {
+                            it.copy(
+                                currentQuestion = currentQuestion + 1,
+                                question = getQuizUseCase.invoke().get(currentQuestion).question,
+                                answers = getQuizUseCase.invoke().get(currentQuestion).answers
+
+                            )
+                        }
+
+                    }
+
+                }
+            },
+            interval = interval
+        )
+    }
+
+    fun stopTimer() {
+        countdownTimerUseCase.stopTimer()
+    }
+
+    private companion object {
+        const val totalTime = 5000L
+        const val interval = 1000L
+    }
 }
