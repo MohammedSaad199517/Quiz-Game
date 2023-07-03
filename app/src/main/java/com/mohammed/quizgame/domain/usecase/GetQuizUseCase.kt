5e8693e939ad72d54bc9b6efecc388316d67761b
Mohammed Saad <mohammedsaad20161995@gmail.com>
2023-07-03 22:00:08 +0300

    filter result based on levelSelected and categorySelected

--- app/src/main/java/com/mohammed/quizgame/domain/usecase/GetQuizUseCase.kt
+++ app/src/main/java/com/mohammed/quizgame/domain/usecase/GetQuizUseCase.kt
@@ -1,11 +1,21 @@
 package com.mohammed.quizgame.domain.usecase
 
 import com.mohammed.quizgame.data.repository.quiz.QuizRepository
+import com.mohammed.quizgame.domain.models.Game
 import com.mohammed.quizgame.utils.toGame
 import javax.inject.Inject
 
 class GetQuizUseCase @Inject constructor(
-    private val quizRepository: QuizRepository
+    private val quizRepository: QuizRepository,
+    private val getSavedConfigurationUseCase: GetSavedConfigurationUseCase
 ) {
-    suspend operator fun invoke() = quizRepository.getAllCachedQuestions().map { it.toGame() }
+    suspend operator fun invoke(): List<Game> {
+        val levelSelected = getSavedConfigurationUseCase.invoke()?.selectedLevel!!
+        val categorySelected = getSavedConfigurationUseCase.invoke()?.selectedCategory!!
+        return quizRepository.getAllCachedQuestions()
+            .map { it.toGame() }
+            .filter {
+                it.difficulty == levelSelected && it.category == categorySelected
+            }
+    }
 }
