696c99fe46b3a155a7169b261e10b28f4c2bcdb9
Mohammed Saad <mohammedsaad20161995@gmail.com>
2023-07-03 11:42:05 +0300

    update CurrentScore function

--- app/src/main/java/com/mohammed/quizgame/composable/CurrentScore.kt
+++ app/src/main/java/com/mohammed/quizgame/composable/CurrentScore.kt
@@ -1,39 +1,39 @@
 package com.mohammed.quizgame.composable
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.mohammed.quizgame.ui.theme.mainColor
-import com.mohammed.quizgame.ui.theme.white87
+import com.mohammed.quizgame.ui.theme.Gold87
+import com.mohammed.quizgame.ui.theme.MainColor
+import com.mohammed.quizgame.ui.theme.White87
+
 
 @Composable
 fun CurrentScore() {
-    Row(
-        modifier = Modifier
-            .padding(16.dp)
-            .background(mainColor, RoundedCornerShape(16.dp))
-            .padding(8.dp)
+    Card(
+        modifier = Modifier.fillMaxWidth(),
+        colors = CardDefaults.cardColors(MainColor),
+        shape = RectangleShape
     ) {
-        Box() {
-            Text(
-                text = "Score :",
-                color = white87,
-                fontSize = 16.sp
-            )
-        }
-
         Text(
-            text = "2",
-            color = white87,
-            fontSize = 16.sp,
-            modifier = Modifier.padding(start = 8.dp)
+            modifier = Modifier
+                .padding(16.dp)
+                .background(Gold87, RoundedCornerShape(16.dp))
+                .padding(8.dp),
+            text = "Score : 2",
+            color = White87,
+            fontSize = 16.sp
         )
     }
+
+
 }
