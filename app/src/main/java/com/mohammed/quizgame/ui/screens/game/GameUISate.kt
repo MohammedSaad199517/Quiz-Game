e0378e1ea960262655b6cfcadbb128ea3047bf32
Mohammed Saad <mohammedsaad20161995@gmail.com>
2023-07-03 11:46:32 +0300

    use hashmap

--- app/src/main/java/com/mohammed/quizgame/ui/screens/game/GameUISate.kt
+++ app/src/main/java/com/mohammed/quizgame/ui/screens/game/GameUISate.kt
@@ -2,12 +2,11 @@ package com.mohammed.quizgame.ui.screens.game
 
 data class GameUISate(
     val question: String = "",
-    val time: Int = 0,
+    val time: Long = 0L,
     val totalQuestions: Int = 0,
-    val currentQuestion: Int = 0,
+    val currentQuestion: Int = 1,
     val currentScore: Int = 0,
-    val correctAnswer: String = "",
-    val wrongAnswer: List<String> = emptyList(),
+    val answers: List<HashMap<String, String>> = emptyList(),
     val isLoading: Boolean = true,
     val errors: String? = null
 
