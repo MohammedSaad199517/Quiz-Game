b65260d600e803954a82592a4e29f81505072b46
Mohammed Saad <mohammedsaad20161995@gmail.com>
2023-07-04 09:18:56 +0300

    use lottie animation image in SplashScreen

--- app/src/main/java/com/mohammed/quizgame/ui/screens/splash/SplashScreen.kt
+++ app/src/main/java/com/mohammed/quizgame/ui/screens/splash/SplashScreen.kt
@@ -3,17 +3,25 @@ package com.mohammed.quizgame.ui.screens.splash
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.rememberLottieComposition
+import com.mohammed.quizgame.R
 import com.mohammed.quizgame.Screen
 import kotlinx.coroutines.delay
 
 @Composable
 fun SplashScreen (navController: NavController){
+    val composition by rememberLottieComposition(spec = LottieCompositionSpec.RawRes(R.raw.splash_lottie))
+    
     LaunchedEffect(key1 =true  ){
         delay(5000L)
         navController.navigate(Screen.ConfigurationScreen.route)
@@ -24,7 +32,11 @@ fun SplashScreen (navController: NavController){
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "Splash screen")
+        LottieAnimation(
+            composition = composition,
+            modifier = Modifier.size(400.dp)
+        )
+
         
     }
 }
