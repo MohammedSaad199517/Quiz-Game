9a258040325de022beefabf38bfb45ce2e6a61f6
Mohammed Saad <mohammedsaad20161995@gmail.com>
2023-07-04 16:11:07 +0300

    handle score value

--- app/src/main/java/com/mohammed/quizgame/ui/screens/winner/WinnerScreen.kt
+++ app/src/main/java/com/mohammed/quizgame/ui/screens/winner/WinnerScreen.kt
@@ -6,19 +6,27 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.mohammed.quizgame.composable.PlayAgainButton
 import com.mohammed.quizgame.composable.WinningCup
 
 @Composable
-fun WinnerScreen(navController: NavController) {
+fun WinnerScreen(
+    navController: NavController,
+    viewModel: WinnerViewModel = hiltViewModel()
+) {
+    val uiState by viewModel.uiState.collectAsState()
+
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier.padding(16.dp)
@@ -31,7 +39,7 @@ fun WinnerScreen(navController: NavController) {
             color = Color.Green
         )
         Spacer(modifier = Modifier.size(16.dp))
-        Text(text = "score achieved : 5")
+        Text(text = "score achieved : ${uiState.scoreAchieved}")
         Spacer(modifier = Modifier.size(16.dp))
         PlayAgainButton(navController)
 
