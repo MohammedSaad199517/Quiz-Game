d158f57a79fa5ef0d30797ef6fba727193d8ee03
Mohammed Saad <mohammedsaad20161995@gmail.com>
2023-07-04 10:15:22 +0300

    make lottie animation iterate forever

--- app/src/main/java/com/mohammed/quizgame/ui/screens/splash/SplashScreen.kt
+++ app/src/main/java/com/mohammed/quizgame/ui/screens/splash/SplashScreen.kt
@@ -7,26 +7,31 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.airbnb.lottie.compose.LottieAnimation
 import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
 import com.airbnb.lottie.compose.rememberLottieComposition
 import com.mohammed.quizgame.R
 import com.mohammed.quizgame.Screen
 import kotlinx.coroutines.delay
 
 @Composable
-fun SplashScreen (navController: NavController){
+fun SplashScreen(navController: NavController) {
     val composition by rememberLottieComposition(spec = LottieCompositionSpec.RawRes(R.raw.splash_lottie))
-    
-    LaunchedEffect(key1 =true  ){
-        delay(5000L)
-        navController.navigate(Screen.ConfigurationScreen.route)
+    val isPlaying by remember { mutableStateOf(true) }
+    val progress by animateLottieCompositionAsState(
+        composition = composition,
+        isPlaying = isPlaying,
+        iterations = LottieConstants.IterateForever
+    )
 
-    }
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
@@ -34,9 +39,15 @@ fun SplashScreen (navController: NavController){
     ) {
         LottieAnimation(
             composition = composition,
-            modifier = Modifier.size(400.dp)
+            modifier = Modifier.size(400.dp),
+            progress = { progress }
         )
-
-        
     }
+
+    LaunchedEffect(key1 = true) {
+        delay(5000L)
+        navController.navigate(Screen.ConfigurationScreen.route)
+
+    }
+
 }
