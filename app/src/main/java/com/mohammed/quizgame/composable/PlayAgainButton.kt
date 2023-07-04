07d7ad65c046dee3f9fcbaba8c697cc06c26ac5a
Mohammed Saad <mohammedsaad20161995@gmail.com>
2023-07-04 14:17:09 +0300

    add PlayAgainButton Composable function

---
+++ app/src/main/java/com/mohammed/quizgame/composable/PlayAgainButton.kt
@@ -0,0 +1,23 @@
+package com.mohammed.quizgame.composable
+
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import com.mohammed.quizgame.Screen
+import com.mohammed.quizgame.ui.theme.MainColor
+
+@Composable
+fun PlayAgainButton(navController: NavController) {
+    Button(
+        onClick = {
+            navController.navigate(Screen.ConfigurationScreen.route)
+        },
+        colors = ButtonDefaults.buttonColors(MainColor),
+
+        ) {
+        Text(text = "play again")
+
+    }
+}
