b4f9f5e99ba9167f4b9428a26d0be944efd6e0d7
Mohammed Saad <mohammedsaad20161995@gmail.com>
2023-07-04 16:10:23 +0300

    send argument to winner screen

--- app/src/main/java/com/mohammed/quizgame/ui/screens/winner/WinnerRoute.kt
+++ app/src/main/java/com/mohammed/quizgame/ui/screens/winner/WinnerRoute.kt
@@ -2,10 +2,15 @@ package com.mohammed.quizgame.ui.screens.winner
 
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavType
 import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
 import com.mohammed.quizgame.Screen
 
-fun NavGraphBuilder.winnerRoute (navController: NavController){
-    composable(Screen.WinnerScreen.route){ WinnerScreen(navController = navController)}
-    
+fun NavGraphBuilder.winnerRoute(navController: NavController) {
+    composable(
+        route = "${Screen.WinnerScreen.route}/{score}",
+        arguments = listOf(navArgument("score") { NavType.StringType })
+    ) { WinnerScreen(navController = navController) }
+
 }
