5fe1dbe6e4b5d70ae53b39e6648b37c8f44a38a5
Mohammed Saad <mohammedsaad20161995@gmail.com>
2023-07-04 14:21:09 +0300

    add WinningCup Composable function

---
+++ app/src/main/java/com/mohammed/quizgame/composable/WinningCup.kt
@@ -0,0 +1,41 @@
+package com.mohammed.quizgame.composable
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
+import com.mohammed.quizgame.R
+
+@Composable
+fun WinningCup() {
+    val composition by rememberLottieComposition(spec = LottieCompositionSpec.RawRes(R.raw.win_lottie))
+    val isPlaying by remember { mutableStateOf(true) }
+    val progress by animateLottieCompositionAsState(
+        composition = composition,
+        isPlaying = isPlaying,
+    )
+
+    Column(
+        modifier = Modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        LottieAnimation(
+            composition = composition,
+            modifier = Modifier.size(400.dp),
+            progress = { progress }
+        )
+    }
+
+
+}
