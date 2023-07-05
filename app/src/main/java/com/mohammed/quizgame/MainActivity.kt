a2ed6e088bb5ee7ad91834c748781a0f77e38a53
Mohammed Saad <mohammedsaad20161995@gmail.com>
2023-07-05 11:55:56 +0300

    add QuizGameTheme

--- app/src/main/java/com/mohammed/quizgame/MainActivity.kt
+++ app/src/main/java/com/mohammed/quizgame/MainActivity.kt
@@ -3,6 +3,7 @@ package com.mohammed.quizgame
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.mohammed.quizgame.ui.theme.QuizGameTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -10,7 +11,9 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            QuizGameApp()
+            QuizGameTheme(){
+                QuizGameApp()
+            }
         }
     }
 }
