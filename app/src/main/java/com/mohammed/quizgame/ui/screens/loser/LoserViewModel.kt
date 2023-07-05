ec6910b88e722c1f89faadc1cceb2fadfa1ac030
Mohammed Saad <mohammedsaad20161995@gmail.com>
2023-07-05 15:08:47 +0300

    create LoserViewModel

---
+++ app/src/main/java/com/mohammed/quizgame/ui/screens/loser/LoserViewModel.kt
@@ -0,0 +1,24 @@
+package com.mohammed.quizgame.ui.screens.loser
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import javax.inject.Inject
+
+
+@HiltViewModel
+class LoserViewModel @Inject constructor(
+    savedStateHandle: SavedStateHandle
+
+) : ViewModel() {
+    private val _uiState = MutableStateFlow(LoserUIState())
+    val uiState = _uiState.asStateFlow()
+    private val args: String = checkNotNull(savedStateHandle["score"])
+
+    init {
+        _uiState.update { it.copy(scoreAchieved = args) }
+    }
+}
