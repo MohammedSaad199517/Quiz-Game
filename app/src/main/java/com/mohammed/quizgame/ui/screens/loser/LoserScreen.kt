024646102474b20440018d8fb087fb0b1f21a104
Mohammed Saad <mohammedsaad20161995@gmail.com>
2023-07-05 11:52:10 +0300

    add LoserScreen

---
+++ app/src/main/java/com/mohammed/quizgame/ui/screens/loser/LoserScreen.kt
@@ -0,0 +1,49 @@
+package com.mohammed.quizgame.ui.screens.loser
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.mohammed.quizgame.composable.LottieGameOver
+import com.mohammed.quizgame.composable.PlayAgainButton
+
+@Composable
+fun LoserScreen (navController: NavController){
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.padding(16.dp)
+
+    ) {
+        LottieGameOver()
+        Text(
+            text = "Game Over",
+            fontSize = 32.sp,
+            color = Color.Red
+        )
+        Spacer(modifier = Modifier.size(16.dp))
+        Text(text = "score achieved : ")
+        Spacer(modifier = Modifier.size(16.dp))
+        PlayAgainButton(navController)
+
+
+    }
+
+}
+@Composable
+@Preview(showSystemUi = true)
+fun PreviewLoserScreen(){
+    val navController = rememberNavController()
+    LoserScreen (navController)
+
+}
