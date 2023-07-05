4ad704f8b74d1eac1c5d280d629d5ad72b6025ab
Mohammed Saad <mohammedsaad20161995@gmail.com>
2023-07-05 15:10:39 +0300

    handle getting arguments in LoserScreen

--- app/src/main/java/com/mohammed/quizgame/ui/screens/loser/LoserScreen.kt
+++ app/src/main/java/com/mohammed/quizgame/ui/screens/loser/LoserScreen.kt
@@ -6,19 +6,26 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.mohammed.quizgame.composable.LottieGameOver
 import com.mohammed.quizgame.composable.PlayAgainButton
 
 @Composable
-fun LoserScreen (navController: NavController){
+fun LoserScreen (
+    navController: NavController,
+    viewModel: LoserViewModel= hiltViewModel()
+){
+    val uiState by viewModel.uiState.collectAsState()
 
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -32,7 +39,7 @@ fun LoserScreen (navController: NavController){
             color = Color.Red
         )
         Spacer(modifier = Modifier.size(16.dp))
-        Text(text = "score achieved : ")
+        Text(text = "score achieved : ${uiState.scoreAchieved}")
         Spacer(modifier = Modifier.size(16.dp))
         PlayAgainButton(navController)
 
--- app/src/main/java/com/mohammed/quizgame/ui/screens/loser/loserRoute.kt
+++ app/src/main/java/com/mohammed/quizgame/ui/screens/loser/loserRoute.kt
@@ -2,10 +2,16 @@ package com.mohammed.quizgame.ui.screens.loser
 
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavType
 import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
 import com.mohammed.quizgame.Screen
 
-fun NavGraphBuilder.loserRoute(navController: NavController){
-    composable(Screen.LoserScreen.route){ LoserScreen(navController)}
+fun NavGraphBuilder.loserRoute(navController: NavController) {
+
+    composable(
+        route = "${Screen.LoserScreen.route}/{score}",
+        arguments = listOf(navArgument("score") { NavType.StringType })
+    ) { LoserScreen(navController = navController) }
 
 }
