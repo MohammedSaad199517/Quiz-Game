3b0333fb6b5c2965f36b89045f387184c34fc191
Mohammed Saad <mohammedsaad20161995@gmail.com>
2023-07-06 21:36:05 +0300

    update SavedConfiguration

--- app/src/main/java/com/mohammed/quizgame/data/repository/configuration/ConfigurationRepository.kt
+++ app/src/main/java/com/mohammed/quizgame/data/repository/configuration/ConfigurationRepository.kt
@@ -3,11 +3,11 @@ package com.mohammed.quizgame.data.repository.configuration
 
 interface ConfigurationRepository {
     suspend fun saveConfiguration(selectedCategory: String?, selectedLevel: String?, selectedQuantity: Int?)
-    suspend fun getSavedConfiguration(): SavedConfiguration?
+    suspend fun getSavedConfiguration(): SavedConfiguration
 
 }
 data class SavedConfiguration(
-    val selectedCategory: String,
-    val selectedLevel: String,
-    val selectedQuantity: Int
+    val selectedCategory: String?,
+    val selectedLevel: String?,
+    val selectedQuantity: Int?
 )
