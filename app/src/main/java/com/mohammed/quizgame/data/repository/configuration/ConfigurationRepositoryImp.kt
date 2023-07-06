8fededf99094c84c392ff82e2a3dd04683fe8b4e
Mohammed Saad <mohammedsaad20161995@gmail.com>
2023-07-06 21:37:22 +0300

    update ConfigurationRepositoryImp

--- app/src/main/java/com/mohammed/quizgame/data/repository/configuration/ConfigurationRepositoryImp.kt
+++ app/src/main/java/com/mohammed/quizgame/data/repository/configuration/ConfigurationRepositoryImp.kt
@@ -3,38 +3,38 @@ package com.mohammed.quizgame.data.repository.configuration
 import android.content.Context
 import android.content.SharedPreferences
 
-class ConfigurationRepositoryImp (private val context: Context) :ConfigurationRepository{
-
-    override suspend fun saveConfiguration(selectedCategory: String?, selectedLevel: String?, selectedQuantity: Int?) {
-        val editor = sharedPreferences.edit()
-
-        if (selectedQuantity != null){
-            editor.putInt(KEY_QUANTITY, selectedQuantity)
-        }
-        if (selectedLevel != null){
-            editor.putString(KEY_LEVEL, selectedLevel)
-        }
-        if (selectedCategory != null){
-            editor.putString(KEY_CATEGORY, selectedCategory)
-        }
-        editor.apply()
-    }
+class ConfigurationRepositoryImp(private val context: Context) : ConfigurationRepository {
 
     private val sharedPreferences: SharedPreferences by lazy {
         context.getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE)
     }
 
+    override suspend fun saveConfiguration(
+        selectedCategory: String?,
+        selectedLevel: String?,
+        selectedQuantity: Int?
+    ) {
+        val editor = sharedPreferences.edit()
+
+        if (selectedQuantity != null) {
+            editor.putInt(KEY_QUANTITY, selectedQuantity)
+        }
+        if (selectedLevel != null) {
+            editor.putString(KEY_LEVEL, selectedLevel)
+        }
+        if (selectedCategory != null) {
+            editor.putString(KEY_CATEGORY, selectedCategory)
+        }
+        editor.apply()
+    }
 
 
-    override suspend fun getSavedConfiguration(): SavedConfiguration? {
+    override suspend fun getSavedConfiguration(): SavedConfiguration {
         val category = sharedPreferences.getString(KEY_CATEGORY, null)
         val level = sharedPreferences.getString(KEY_LEVEL, null)
         val quantity = sharedPreferences.getInt(KEY_QUANTITY, -1)
-        return if (category != null && level != null && quantity != -1) {
-            SavedConfiguration(category, level, quantity)
-        } else {
-            null
-        }
+        return SavedConfiguration(category, level, quantity)
+
     }
 
     private companion object {
